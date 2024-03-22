39b6405e82cb330eeb05c96610bbcd3453fa260a
Luiz-elTlaloc <el.tlaloc1316@gmail.com>
2024-03-22 15:42:37 -0600

    mini-project-done

--- server/app.js
+++ server/app.js
@@ -4,12 +4,13 @@ var logger = require("morgan");
 const mongoose = require("mongoose");
 var cors = require("cors");
 
-
+// var indexRouter = require('./router/index')
 var usersRouter = require("./routes/users");
 var authRouter = require("./routes/auth");
 var apiRouter = require("./routes/api");
 var studentsRouter = require("./routes/students");
 var cohortsRouter = require("./routes/cohorts");
+var authRouter =require("./routes/auth")
 var app = express();
 
 
@@ -25,10 +26,14 @@ app.use(
   })
 );
 
+
+// app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/auth", authRouter);
 app.use("/students", studentsRouter);
 app.use("/cohorts", cohortsRouter);
+app.use('/auth', authRouter);
+
 
 
 mongoose
